f9a4fc3dca6662aec84657518fa74143b3381f38
V Suhas <suhas3145@gmail.com>
2023-07-21 04:29:38 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-
-
+import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import Sidebar from "./components/sidebar/Sidebar"
-import Home from "./pages/home/Home"
-import "./app.css"
-
+import "./app.css";
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 function App() {
   return (
-    <div>
-
-      <Topbar/>
+    <Router>
+      <Topbar />
       <div className="container">
-        <Sidebar/>
-        <Home/>
-      </div>
+        <Sidebar />
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
 
-    </div>
-    
-    
-      
-    
+          <Route path="/users" element={<UserList />}/>
+            
+          <Route path="/user/:userId" element={<User />}/>
+            
+          <Route path="/newUser" element={<NewUser />}/>
+            
+          <Route path="/products" element={<ProductList />}/>
+            
+          <Route path="/product/:productId" element={<Product />}/>
+            
+          <Route path="/newproduct" element={<NewProduct />}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
