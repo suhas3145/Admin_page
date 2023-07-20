1ba5189cb31c873300ccbc7a085f85a4a32e2291
V Suhas <suhas3145@gmail.com>
2023-07-21 03:49:21 +0530

    added new changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,17 @@ import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css"
 
 import {userData} from "../../dummyData"
-
+import WidgetSm from "../../components/WidgetSm/WidgetSm"
+import WidgetLg from "../../components/widgetLg/WidgetLg"
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo/>
       <Chart data={userData} title="User Analytics"  grid dataKey="Active User"/>
+      <div className="homeWidgets">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
     </div>
   )
 }
